0fcc0ccf89d6c11e723bf385287f38e26a938877
Joao Coelho <joaocoelho93@gmail.com>
2019-07-13 17:51:07 -0400

    Initial home page with title and container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainContainer from './components/mainContainer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-title">
+        João's
+        <div className="the-score-text">
+          the<span className="score-s">S</span>core
+        </div>
+        App
+      </div>
+      <MainContainer />
     </div>
   );
 }
---
+++ src/components/mainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContainer() {
+  return (
+    <div className="main-grid">
+      This is a test
+    </div>
+  );
+}
+
+export default MainContainer;
